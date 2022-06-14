daf4a53a79fe843a210ac8ae2baaa75cc60e0233
Douglas Obara <douglasobara97@gmail.com>
2022-06-14 21:40:07 +0300

    commit tree 4e4c952100ba3ccf86954f9d28775ff489ee20a5

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  //  sends a fetch request to 'https://anapioficeandfire.com/api/books'
+  // renders book titles into the DOM by passing a JSON object to renderBooks()
+  const fetchBooks = fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((jsonData) => renderBooks(jsonData));
+  return fetchBooks;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
